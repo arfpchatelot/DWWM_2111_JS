e89f7d20308aad9b04e7f17989d35f34ef647406
arfpchatelot <fchatelot@yahoo.fr>
2022-04-25 11:38:05 +0200

    Variateur de couleurs avec controle de saisie

--- exo_couleurs/couleur.js
+++ exo_couleurs/couleur.js
@@ -4,6 +4,59 @@ function getColor()
 document.body.setAttribute("style","background-color:"+chainecouleur ); 
 //document.body.style.backgroundColor()
 }
+
+
+const rouge= document.getElementById("rouge");
+const vert=document.getElementById("vert");
+const bleu=document.getElementById("bleu");
+
+function verifChamps ( _champs) 
+{ let testbool=false;
+    let chaine= _champs.value.toUpperCase();
+let regex=/^[0-9A-F]{2}$/;
+if (chaine.match( regex)) {
+
+testbool=true;
+    
+} else {
+
+    _champs.value="";
+    _champs.focus();
+
+}
+return testbool;
+
+} 
+
+document.querySelector("#rouge").addEventListener("blur", function() {
+
+
+if (verifChamps(rouge)) {
+    getColor();
+    }
+
+})
+
+document.querySelector("#vert").addEventListener("blur", function() {
+
+
+    if (verifChamps(vert)) {
+        getColor();
+        }
+    
+    })
+    
+    document.querySelector("#bleu").addEventListener("blur", function() {
+
+
+        if (verifChamps(bleu)) {
+            getColor();
+            }
+        
+        })
+
+
+
 let btnrouge= document.querySelector("#btnrouge");
 
 btnrouge.addEventListener("click", function () {
@@ -14,3 +67,23 @@ btnrouge.addEventListener("click", function () {
     getColor();
 
 });
+
+let btnvert=document.querySelector("#btnvert");
+btnvert.addEventListener("click", function () {
+ document.querySelector("#rouge").value="00";
+ document.querySelector("#vert").value="ff";
+document.querySelector("#bleu").value="00";
+getColor();
+
+})
+let btnbleu=document.querySelector("#btnbleu");
+btnbleu.addEventListener("click", function () {
+ document.querySelector("#rouge").value="00";
+ document.querySelector("#vert").value="00";
+document.querySelector("#bleu").value="ff";
+getColor();
+
+})
+
+
+
